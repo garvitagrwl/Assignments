2b1c76ef039613afa243a3ba7060c8f8b801c790
Garvit Agrawal <garvitagrawal321.delhi@gmail.com>
2026-05-31 19:10:18 +0530

    Empty repository

--- Assignment_10/Question_4.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-arr = np.array([[1, -2, 3], [-4, 5, -6]])
-arr[arr < 0] = 0
-
-print("Q4 Result:\n", arr)
--- Assignment_8/Question_2.py
+++
@@ -1,3 +0,0 @@
-import numpy as np
-d= np.arange(2, 11).reshape(3, 3)
-print("\n3x3 Matrix with values from 2 to 10:\n", d)
